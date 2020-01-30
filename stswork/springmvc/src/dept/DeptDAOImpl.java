df090566219c621826e8860fec2bc75846ae0c98
unknown <wishinghyun@naver.com>
2020-01-30 20:08:55 +0900

    spring

---
+++ stswork/springmvc/src/dept/DeptDAOImpl.java
@@ -0,0 +1,36 @@
+package dept;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+//사용자정의 dao클래스
+//bean으로 만드는 @Repository
+@Repository
+public class DeptDAOImpl {
+	@Autowired
+	JdbcTemplate mytemplate;
+	public int count() {
+		return mytemplate.queryForObject("select count(*) from dept", Integer.class);
+	}
+	
+	public int insert(String deptno, String deptname) {
+		String sql = 
+				"insert into dept(deptno,deptname) values(?,?)";
+		int result = mytemplate.update(sql, deptno,deptname);
+		return result;
+	}
+	
+	public int update(String deptno, String deptname) {
+		String sql = "update dept set deptname = ? where deptno = ?";
+		int result = mytemplate.update(sql, deptname,deptno);
+		return result;
+	}
+	
+	public List<DeptDTO> selectAll(){
+		return mytemplate.query("select deptno,deptname from dept", 
+														new MyDeptRowMapper());
+	}
+}
